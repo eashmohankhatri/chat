2138206c5b5b6180ace4dda4fdb56fb32411e7f6
eashmohankhatri <erickeash2002@gmail.com>
2021-09-27 15:02:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const io = require('socket.io')(server);
 let connectedPeers = [];
